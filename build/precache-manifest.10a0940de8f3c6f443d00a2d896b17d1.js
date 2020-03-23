de6171eca0adfb3d989eeb4c7b02de76e832a6e7
Herman Lin <herman.lin@foxmail.com>
2020-03-22 19:37:23 -0700

    Just the build folder

---
+++ build/precache-manifest.10a0940de8f3c6f443d00a2d896b17d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253f465bc043ca606dcf5c8478285546",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51203ee767008c683282",
+    "url": "./static/css/main.56d10bde.chunk.css"
+  },
+  {
+    "revision": "5de028da6d5f05f2eeaa",
+    "url": "./static/js/2.e847c5b2.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "./static/js/2.e847c5b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51203ee767008c683282",
+    "url": "./static/js/main.fa43a380.chunk.js"
+  },
+  {
+    "revision": "870d72bb829f7eec1eed",
+    "url": "./static/js/runtime-main.f133b5f5.js"
+  }
+]);
